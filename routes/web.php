9b5fbe80dd85d23ca13b072f5b26f1c25cfb5bc2
mayanksolanki5 <mayanksolanki5401@gmail.com>
2022-03-30 11:09:35 +0530

    before commit

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'UserController@index')->name('index');
 
+Auth::routes();
 
-
-
+Route::get('/home', 'HomeController@index')->name('home');
